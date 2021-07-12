85ac0de7fb675bd6fdf00c600054d39a92212a26
Alex_Kravecas <akravecas@yahoo.com>
2021-07-12 10:33:12 -0400

    ruby working

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+
+Product.destroy_all
+Seller.destroy_all
+
+5.times do
+    Seller.create(name: Faker::Name.name, company: Faker::Company.name)
+end
